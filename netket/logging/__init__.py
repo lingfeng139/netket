744d43b5c100c3f9ce04d5ae91512bde5a3ce7c8
Filippo Vicentini <filippovicentini@gmail.com>
2020-09-22 11:16:26 +0200

    Fix sacred importing

--- netket/logging/__init__.py
+++ netket/logging/__init__.py
@@ -5,11 +5,12 @@ from ..utils import tensorboard_available
 if tensorboard_available:
     from ._tensorboard import TBLog
 
-
 try:
     import sacred
 
     sacred_available = True
-    from ._sacred import SacredLog
 except ImportError:
-    pass
+    sacred_available = False
+
+if sacred_available:
+    from ._sacred import SacredLog
